7f3cd0799e4e981831ab9075840069c047854fcc
Paul Reimer <paul@p-rimes.net>
2017-04-04 14:06:54 -0700

    Remove unused symlinks after installing may create them, and ensure ~/.ssh/known_hosts directory and file exists in install.sh

--- install.sh
+++ install.sh
@@ -6,3 +6,11 @@ for path in ${thisdir}/*; do
   file=$(basename "$path")
   ln -sf "${path}" ".${file}"
 done
+
+rm \
+	".Molokai.itermcolors" \
+	".Molokai.terminal" \
+	".Roboto Mono for Powerline.ttf"
+
+mkdir -p ~/.ssh
+touch ~/.ssh/known_hosts
