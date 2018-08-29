47a4fe95289fac3ccef25fdfbfef8a2cc6663d26
Paul Reimer <paul@p-rimes.net>
2018-08-29 11:10:26 -0700

    🎨 Move machine-specific config to end of fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -10,13 +10,13 @@ test -f ".zlogin" || curl --silent -L "https://dotfiles.p-rimes.net/zlogin" -o "
 test -f ".zshrc" || curl --silent -L "https://dotfiles.p-rimes.net/zshrc" -o ".zshrc"
 test -d ".zsh" || git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ".zsh/zsh-syntax-highlighting"
 
-# machine-specific configuration
-# (this is a macOS current workstation default):
-#test -f ".zshenv" || curl --silent -L "https://dotfiles.p-rimes.net/zshenv" -o ".zshenv"
-
 # vim/nvim
 test -f ".vimrc" || curl --silent -L "https://dotfiles.p-rimes.net/vimrc" -o ".vimrc"
 test -f ".config/nvim/init.vim" || curl --create-dirs --silent -L "https://dotfiles.p-rimes.net/init.vim" -o ".config/nvim/init.vim"
 test -d ".vim" || curl -fLo .vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 command -v nvim && nvim +'PlugInstall --sync' +qa
+
+# machine-specific configuration
+# (this is a macOS current workstation default):
+#test -f ".zshenv" || curl --silent -L "https://dotfiles.p-rimes.net/zshenv" -o ".zshenv"
