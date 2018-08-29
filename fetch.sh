b329d6b858d77791a208a3ae30ff79d4ecdf28a4
Paul Reimer <paul@p-rimes.net>
2018-08-29 11:06:40 -0700

    🎨 Group dotfiles into sections in fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,13 +4,16 @@
 test -f ".gitconfig" || curl --silent -L "https://dotfiles.p-rimes.net/gitconfig" -o ".gitconfig"
 test -f ".htoprc" || curl --silent -L "https://dotfiles.p-rimes.net/htoprc" -o ".htoprc"
 test -f ".screenrc" || curl --silent -L "https://dotfiles.p-rimes.net/screenrc" -o ".screenrc"
-test -f ".vimrc" || curl --silent -L "https://dotfiles.p-rimes.net/vimrc" -o ".vimrc"
+
+# zsh
 test -f ".zlogin" || curl --silent -L "https://dotfiles.p-rimes.net/zlogin" -o ".zlogin"
 test -f ".zshrc" || curl --silent -L "https://dotfiles.p-rimes.net/zshrc" -o ".zshrc"
 # machine-specific configuration
-# (this is a macOS user default):
+# (this is a macOS current workstation default):
 #test -f ".zshenv" || curl --silent -L "https://dotfiles.p-rimes.net/zshenv" -o ".zshenv"
 
-test -d ".vim" || curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+# vim/nvim
+test -f ".vimrc" || curl --silent -L "https://dotfiles.p-rimes.net/vimrc" -o ".vimrc"
+test -d ".vim" || curl -fLo .vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 command -v nvim && nvim +'PlugInstall --sync' +qa
