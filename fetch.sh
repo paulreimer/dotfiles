8e0b5aa9674d0f6de52e8141c7a0474dcaf3adde
Paul Reimer <paul@p-rimes.net>
2018-08-29 10:48:35 -0700

    ♻️ Remove environment variables from fetch.sh
    Use hard-coded https://dotfiles.p-rimes.net and output files to current directory (if not exists)

--- fetch.sh
+++ fetch.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-AUTHORITY="https://dotfiles.p-rimes.net"
-D="${HOME}"
-
 # dotfiles
-test -f "${D}/.gitconfig" || curl --silent -L "${AUTHORITY}/gitconfig" -o "${D}/.gitconfig"
-test -f "${D}/.htoprc" || curl --silent -L "${AUTHORITY}/htoprc" -o "${D}/.htoprc"
-test -f "${D}/.screenrc" || curl --silent -L "${AUTHORITY}/screenrc" -o "${D}/.screenrc"
-test -f "${D}/.vimrc" || curl --silent -L "${AUTHORITY}/vimrc" -o "${D}/.vimrc"
-test -f "${D}/.zlogin" || curl --silent -L "${AUTHORITY}/zlogin" -o "${D}/.zlogin"
-test -f "${D}/.zshrc" || curl --silent -L "${AUTHORITY}/zshrc" -o "${D}/.zshrc"
-#test -f "${D}/.zshenv" || curl --silent -L "${AUTHORITY}/zshenv" -o "${D}/.zshenv"
+test -f ".gitconfig" || curl --silent -L "https://dotfiles.p-rimes.net/gitconfig" -o ".gitconfig"
+test -f ".htoprc" || curl --silent -L "https://dotfiles.p-rimes.net/htoprc" -o ".htoprc"
+test -f ".screenrc" || curl --silent -L "https://dotfiles.p-rimes.net/screenrc" -o ".screenrc"
+test -f ".vimrc" || curl --silent -L "https://dotfiles.p-rimes.net/vimrc" -o ".vimrc"
+test -f ".zlogin" || curl --silent -L "https://dotfiles.p-rimes.net/zlogin" -o ".zlogin"
+test -f ".zshrc" || curl --silent -L "https://dotfiles.p-rimes.net/zshrc" -o ".zshrc"
+# machine-specific configuration
+# (this is a macOS user default):
+#test -f ".zshenv" || curl --silent -L "https://dotfiles.p-rimes.net/zshenv" -o ".zshenv"
