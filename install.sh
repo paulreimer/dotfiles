4487edf7943eae61aefe1b8f16c147c42498cc3c
Paul Reimer <paul@p-rimes.net>
2019-03-07 12:17:47 -0800

    ♻️ Refactor install.sh to avoid creating needless links

--- install.sh
+++ install.sh
@@ -2,18 +2,27 @@
 
 thisdir=$(dirname "$0")
 
-for path in ${thisdir}/*; do
-  file=$(basename "$path")
-  ln -sfn "${path}" ".${file}"
+for path in "${thisdir}"/*; do
+  file="$(basename "$path")"
+  link=".${file}"
+  if [ ! -e "${link}" ]; then
+    case "$file" in
+    # Skip these files:
+    "Molokai.itermcolors") ;;
+    "Molokai.terminal") ;;
+    "Monokai.xccolortheme") ;;
+    "Roboto Mono for Powerline.ttf") ;;
+    # Otherwise, create a link
+    *)
+      echo "-> ${link}"
+      ln -sn "${path}" "${link}"
+      ;;
+    esac
+  else
+    echo "Skipping ${link}"
+  fi
 done
 
-rm \
-  ".Molokai.itermcolors" \
-  ".Molokai.terminal" \
-  ".Monokai.xccolortheme" \
-  ".Roboto Mono for Powerline.ttf"
-
 mkdir -p ~/bin
-mkdir -p ~/.vim/autoload
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
