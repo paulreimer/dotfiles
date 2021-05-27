1e57c16f99826da1af732d70324357788cdc1d54
Paul Reimer <paul@p-rimes.net>
2021-05-26 17:20:33 -0700

    ⬆️ Upgrade neovim overlay to latest nightly

--- overlays/neovim-overlay.nix
+++ overlays/neovim-overlay.nix
@@ -8,8 +8,8 @@ self: super: {
     src = super.pkgs.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = "1caf58578c6e4b30c18fa56804dd905b5472b460"; # 2021-02-20
-      sha256 = "1ji77myn7h8l725d7vgrp71kswvy8k6iy45a6rab7dmxh8jpqk1j";
+      rev = "a282a177d3320db25fa8f854cbcdbe0bc6abde7f"; # 2021-06-26
+      sha256 = "03ckzr75jf1mps0y3v9rj2jifsfapkpqyxaw1rs5ify9zdmja593";
     };
   });
 }
