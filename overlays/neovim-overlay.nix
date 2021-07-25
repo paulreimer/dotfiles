e272ddcf4c3018c704da6ba154c338e0a331a22e
Paul Reimer <paul@p-rimes.net>
2021-07-24 18:18:50 -0700

    📌 Use neovim v0.5.0 in nix overlay

--- overlays/neovim-overlay.nix
+++ overlays/neovim-overlay.nix
@@ -1,6 +1,6 @@
 self: super: {
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: rec {
-    version = "0.5.0-dev";
+    version = "0.5.0";
     name = "neovim-unwrapped-${version}";
 
     buildInputs = oldAttrs.buildInputs ++ [ super.utf8proc super.tree-sitter ];
@@ -8,8 +8,8 @@ self: super: {
     src = super.pkgs.fetchFromGitHub {
       owner = "neovim";
       repo = "neovim";
-      rev = "a282a177d3320db25fa8f854cbcdbe0bc6abde7f"; # 2021-06-26
-      sha256 = "03ckzr75jf1mps0y3v9rj2jifsfapkpqyxaw1rs5ify9zdmja593";
+      rev = "v0.5.0";
+      sha256 = "0lgbf90sbachdag1zm9pmnlbn35964l3khs27qy4462qzpqyi9fi";
     };
   });
 }
